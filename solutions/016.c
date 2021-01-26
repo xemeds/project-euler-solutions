bfbc62cabab3bf3280155c307a1206e576d5c565
xemeds <xemeds@protonmail.com>
2021-01-26 12:36:30 +0300

    Added p16

---
+++ solutions/016.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <gmp.h>
+
+int main() {
+	mpz_t result;
+	mpz_init(result);
+
+	mpz_ui_pow_ui(result, 2, 1000);
+
+	char result_str[320];
+	mpz_get_str(result_str, 10, result);
+	mpz_clear(result);
+
+	int result_int = 0;
+	for (int i = 0; result_str[i] != '\0'; i++) {
+		result_int += result_str[i] - '0';
+	}
+
+	printf("%i\n", result_int);
+}
